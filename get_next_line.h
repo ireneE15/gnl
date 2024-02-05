1641f599739f4cad9969d31c5e87d7508972dc6c
Irene Escalona Calleja <iescalon@c3r7s3.42urduliz.com>
2024-02-05 11:43:23 +0100

    1

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iescalon <iescalon@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 11:06:29 by iescalon          #+#    #+#             */
+/*   Updated: 2024/02/05 10:16:47 by iescalon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h> // Para operaciones con archivos
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
+
+
+
+#endif
